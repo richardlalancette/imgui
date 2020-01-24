42ce91af379fcf67f63bfa3a828e961103b2d1aa
Richard Lalancette <Richard.Lalancette@youi.tv>
2020-01-24 16:48:35 -0500

    Moved panel to own file

---
+++ examples/example_apple_metal/Shared/YouiDesignSystemPanel.h
@@ -0,0 +1,88 @@
+#ifndef _YOUIDESIGNSYSTEMPANEL_H_
+#define _YOUIDESIGNSYSTEMPANEL_H_
+
+#include "designsystem.h"
+#include "imgui.h"
+#include "imgui_internal.h"
+
+class YouiGui
+{
+public:
+    typedef std::tuple<std::string, int> FontDetailsTupleType;
+
+    youi::DesignSystem gDesignSystem;
+    ImFont *bodyFont = nullptr;
+    ImFont *filebrowserFont = nullptr;
+    ImFont *H1Font = nullptr;
+    ImFont *H2Font = nullptr;
+
+    bool bWindowBrowseFiles = false;
+    bool bDesignSystemLoaded = false;
+    std::vector<std::string> recently_used_files;
+    std::map<FontDetailsTupleType, std::string> gFontToLoadOnNextPast;
+
+public:
+    std::map<std::string, ImFont *> userFonts;
+
+    struct YouiGuiDataModel
+    {
+        struct
+        {
+        } visibility;
+
+        struct
+        {
+            bool visible = true;
+            bool ColorsTabVisible = true;
+            bool TypographyTabVisible = true;
+            bool MotionTabVisible = false;
+            bool LayoutTabVisible = false;
+            bool SettingsTabVisible = true;
+            bool DesignTabVisible = true;
+        } mainAEPanel;
+
+        struct
+        {
+            bool visible;
+        } demoWindow;
+    } m_youiGuiDataModel;
+
+public:
+    void Init();
+    void Render();
+    void RenderMainAEPanel(bool *open);
+    void RenderColorTab(bool *open);
+    void RenderTypographyTab(bool *open);
+    void RenderMotionTab(bool *open);
+    void RenderLayoutTab(bool *open);
+    void RenderSettingsTab(bool *open);
+    void RenderDesignSystemInstructionsTab(bool *open);
+    void RenderEmptyDesignSystemTab();
+
+    void CommandAddLinkedSolid(float *color);
+
+    void SetYouiLightTheme();
+    void SetYouiDarkTheme();
+    bool CheckFileExist(const std::string &path);
+
+    /*
+     * All font added this way is owned by the atlas.
+     */
+    ImFont *LoadUserFont(const FontDetailsTupleType &fontDetails, const std::string &filename);
+
+    /*
+     * All font added this way is owned by the atlas.
+     */
+    ImFont *LoadUserFont(const FontDetailsTupleType &fontDetails, const std::string &filename, const ImFontConfig &fontConfig);
+
+    void DetailsColorButton(const char *text, float col[4], ImGuiColorEditFlags flags);
+    void DisplayNotes(std::shared_ptr<std::vector<std::string>> notes);
+    void TryToOpenDesignSystem(const std::string &designSystemFilename);
+    void TabPageHeader(const std::string &string, ImTextureID textureId);
+    void DetailedColorTooltip(const char *desc, const char *icon = "?", const char *flavorName = "");
+    void TabBar(bool b);
+    void YouiSimpleTooltip(const std::string &tooltipText);
+    void ShowParsingErrorDialog();
+    void FlavorIdentifierDisplay(const std::string &flavorName);
+};
+#endif
